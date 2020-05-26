bf810688ac953e795fafd799083fc3f2f704edfa
unheardof <timheard@LAPTOP-0UPJO6TI.localdomain>
2020-05-26 21:23:23 +0430

    Continued working on initial version of the build_beacon.py script

--- bin/build_beacon.py
+++ bin/build_beacon.py
@@ -5,47 +5,14 @@ import subprocess
 
 def main(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--dst-port", help="The port to call back on", type=int)
-    parser.add_argument("--h", "--dst-host", help="The host to call back to (hostname or IP)", type=string)
+    parser.add_argument("-p", "--dst-port", help="The port to call back on", type=int) # TODO: Add support for a list of ports to try calling back to
+    parser.add_argument("-h", "--dst-host", help="The host to call back to (hostname or IP)", type=string) # TODO: Add support for a list of hostnames to try to call back to
     parser.add_argument("-t", "--client-token", help="Unique identifier for this beacon", type=string)
-    parser.add_argument("-i", "--callback-interval", help="How often the becon should call back (in seconds); defaults to 300 seconds (five minutes)", type=string)
+    parser.add_argument("-c", "--callback-interval", help="How often the beacon should call back (in seconds); defaults to 300 seconds (five minutes)", type=string)
+    parser.add_argument("-b", "--beacon-type", help="What type of beacon you want", type=string)
+    parser.add_argument("-o", "--output-file", help="Where to save the compiled beacon file to", type=string)
+    # TODO: Add target-os argument / add support for cross compiling across multiple platforms
     args = parser.parse_args()
 
+    # TODO: Create build command and then execute it
     subprocess.run([])
-
-    
-    # try:
-    #     opts, args = getopt.getopt(argv, "".join(short_options), long_options)
-    # except getopt.GetoptError:
-    #     print_usage_and_exit(2)
-
-
-    # # TODO: Init variables to None
-        
-    # for opt, arg in opts:
-    #     if opt in ('-h', '--help'):
-    #         print_usage_and_exit(0)
-    #     elif opt in ('-i', '--input-file'):
-    #         if not os.path.isfile(arg):
-    #             print("\nInvalid input file '%s'; file does not exist\n" % arg)
-    #             sys.exit(1)
-                
-    #         input_file = arg
-    #     elif opt in ('-o', '--output-file'):
-    #         output_file = arg
-    #     elif opt in ('-p', '--ports'):
-    #         ports_filter = parse_filter_string(arg)
-    #     elif opt in ('-a', '--ip-addrs'):
-    #         ips_filter = parse_filter_string(arg)
-    #     elif opt in ('-s', '--operating-system'):
-    #         os_filter = parse_filter_string(arg)
-    #     elif opt in ('-d', '--device-type'):
-    #         device_type_filter = parse_filter_string(arg)
-    #     elif opt in ('-c', '--output-csv'):
-    #         output_format = 'csv'
-    #     elif opt in ('-q', '--query-mode'):
-    #         query_mode = True
-    #     else:
-    #         print("\nUnknown option '%s' encountered" % (opt))
-    #         print_usage_and_exit(0)
-
